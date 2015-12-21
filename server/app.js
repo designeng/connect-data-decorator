37dcfb761933ea9750ab975df7598ba440d875f6
designeng <denissavenok@gmail.com>
2015-12-21 16:47:11 +0300

    intends

--- server/app.js
+++ server/app.js
@@ -18,9 +18,9 @@ app.use(express.static('./public'));
 
 /* 404 */
 app.use(function(req, res, next) {
-  var err = new Error('Route is not found!');
-  err.status = 404;
-  next(err);
+    var err = new Error('Route is not found!');
+    err.status = 404;
+    next(err);
 });
 
 module.exports = app;
