5a2de391f05928b6b60406a80c31de586636b478
designeng <denissavenok@gmail.com>
2015-12-21 17:41:02 +0300

    should create react component test inited

--- test/components/connectModel.spec.js
+++ test/components/connectModel.spec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { expect } from 'chai'; 
 
 import ConnectModel from '../../source/connectModel';
 
@@ -20,6 +21,11 @@ describe('connectModel',  () => {
         } catch (error) {
             assert.equal(error.message, 'Falcor model sourcePath should be provided!');
         }
-        
     });
+
+    it('should create react component',  () => {
+        let connect = new ConnectModel({ sourcePath: '/navigation/model.json' });
+        expect(connect instanceof React.Component).to.be(true);
+    });
+
 });
